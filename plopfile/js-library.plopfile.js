c8823809e06a6b0635f335596d57b284f8a5f911
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-11 18:28:09 +0200

    Document coveralls stuff

--- plopfile/js-library.plopfile.js
+++ plopfile/js-library.plopfile.js
@@ -222,8 +222,8 @@ By the way, here are things you’ll have to do by hand:
 
   • Go to https://travis-ci.org/profile/studio-b12 , re-sync your repos
     and switch on CI integration for studio-b12/${answers.name}.
-  • Go to https://coveralls.io/refresh and switch on coveralls
-    for studio-b12/${answers.name}.
+  • Go to https://coveralls.io/refresh , switch on coveralls
+    for studio-b12/${answers.name} and copy the token into \`.coveralls.yml\`.
   • Remember to push your changes to both remotes: \`github\` and \`origin\`.
 
 `
