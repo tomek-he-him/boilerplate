51b8893508cf88304cca910e5008aace7b2aeff1
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-24 14:41:44 +0200

    Get rid of console statement

--- templates/_/customActions/packageJson.js
+++ templates/_/customActions/packageJson.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 module.exports = (params) => () => {
-  console.log('————————————————» Hey!');
   fs.writeFileSync(
     `${params.projectRoot}/package.json`,
     JSON.stringify(params.manifest(params.answers), null, '  ')
