3bea4e8b1b6b1d9074e1e6255bdbda32e1e7cc37
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-24 15:01:45 +0200

    Update tempates

---
+++ templates/command-line-tool/(npm-dev-dependencies).js
@@ -0,0 +1,6 @@
+const merge = require('object-merge');
+const base = require('../js-library/(npm-dev-dependencies)');
+
+module.exports = merge(base, {
+  'marked-man': '0.1.5',
+});
---
+++ templates/command-line-tool/(package-json).js
@@ -0,0 +1,31 @@
+const merge = require('object-merge');
+const base = require('../js-library/(package-json)');
+
+module.exports = (answers) => {
+  const extra = {
+    scripts: {
+      manpages: 'scripts/manpages',
+      prepublish: 'npm run manpages',
+      readme: [
+        'scripts/readme',
+        'git commit -m \'[npm run readme] Update the docs\' Readme.md',
+      ].join(' && '),
+    },
+    bin: {
+      [answers.name]: `bin/${answers.name}`,
+    },
+    files: [
+      '/**/*.js',
+      '!/test.js',
+      '/Readme.md',
+      '/License.md',
+      '/bin/',
+      '/manpages/',
+    ],
+    man: [
+      '/manpages/elm-live.1',
+    ],
+  };
+
+  return merge(extra, base(answers));
+};
