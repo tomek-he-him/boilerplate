ea2ea622f85dbe2d1d7a4d28e54f3e473e4eedfa
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-24 16:22:46 +0200

    Fix dependencies

--- templates/command-line-tool/(npm-dev-dependencies).js
+++ templates/command-line-tool/(npm-dev-dependencies).js
@@ -2,5 +2,6 @@ const merge = require('object-merge');
 const base = require('../js-library/(npm-dev-dependencies)');
 
 module.exports = merge(base, {
+  'commander-to-markdown': '1',
   'marked-man': '0.1.5',
 });
