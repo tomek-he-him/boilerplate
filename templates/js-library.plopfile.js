4dd2f3fcc94fc1ff282a7cc4d0a929f5bf543840
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-24 14:43:36 +0200

    Simplify file tree

--- templates/js-library.plopfile.js
+++ templates/js-library.plopfile.js
@@ -4,13 +4,13 @@ const npmAuthor = require('./_/prompts/npmAuthor');
 const npmKeywords = require('./_/prompts/npmKeywords');
 const confirm = require('./_/prompts/confirm');
 
-const enterProjectRoot = require('./_/customActions/enterProjectRoot');
-const packageJson = require('./_/customActions/packageJson');
-const npmDependencies = require('./_/customActions/npmDependencies');
-const initialCommit = require('./_/customActions/initialCommit');
-const setupGithub = require('./_/customActions/setupGithub');
-const setupSb12 = require('./_/customActions/setupSb12');
-const sayWereDone = require('./_/customActions/sayWereDone');
+const enterProjectRoot = require('./_/actions/enterProjectRoot');
+const packageJson = require('./_/actions/packageJson');
+const npmDependencies = require('./_/actions/npmDependencies');
+const initialCommit = require('./_/actions/initialCommit');
+const setupGithub = require('./_/actions/setupGithub');
+const setupSb12 = require('./_/actions/setupSb12');
+const sayWereDone = require('./_/actions/sayWereDone');
 
 const templates = `${__dirname}/js-library`;
 const devDependencies = require('./js-library/(npm-dev-dependencies)');
