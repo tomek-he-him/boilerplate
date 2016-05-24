abbcdbc02d5a41d90d959056559b7416488436c4
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-24 13:24:21 +0200

    Generate package.json with JS

--- templates/js-library.plopfile.js
+++ templates/js-library.plopfile.js
@@ -13,6 +13,7 @@ const fs = require('fs');
 
 const templates = `${__dirname}/js-library`;
 const devDependencies = require('./js-library/(npm-dev-dependencies)');
+const manifest = require('./js-library/(package-json)');
 
 const renderArg = (arg) => ` ${/\s/.test(arg)
   ? `'${arg.replace(/'/g, '\\\'')}'`
@@ -122,14 +123,7 @@ module.exports = (plop) => {
         'http://npm.im/keyword-popularity might help you out.'
       ),
       filter: (rawKeywords) => (
-        rawKeywords
-          .split(/\s+/)
-          .map(keyword => `\n    "${
-            keyword
-              .replace(/\\/, '\\\\')
-              .replace(/"/, '\\"')
-          }"`)
-          .join(',')
+        rawKeywords.split(/\s+/)
       ),
       validate: (rawKeywords) => (
         /^[a-z-]+(\s+[a-z-]+)*$/.test(rawKeywords)
@@ -185,7 +179,6 @@ git@git.sb12.com:gitolite-config.git .
         'Changelog.yaml',
         'Contributing.md',
         'License.md',
-        'package.json',
         'Readme.md',
         'test.js',
       ].map((filename) => ({
@@ -194,67 +187,84 @@ git@git.sb12.com:gitolite-config.git .
         templateFile: `${templates}/${filename}`,
       }));
 
-      const repoActions = [
-        () => {
-          $('cd', [projectRoot]);
+      // The coming functions are custom actions. Each action has to return
+      // a message (see https://github.com/amwmedia/plop/pull/29).
 
-          // npm dependencies
-          const dependencyStrings =
-            Object.keys(devDependencies).map((dep) => (
-              `${dep}@${devDependencies[dep]}`
-            ));
-          $('npm', ['install', '--save-dev', 'npm']);
-          $(local('npm'), ['install', '--save-dev'].concat(dependencyStrings));
-          $('npm', ['shrinkwrap', '--dev']);
+      const enterProjectRoot = () => {
+        $('cd', [projectRoot]);
+        return 'ok';
+      };
 
-          // Initial commit
-          $('git', ['init']);
-          $('git', ['add', '.']);
-          $('git', ['commit', '--message=Boom!']);
+      const packageJson = () => fs.writeFileSync(
+        `${projectRoot}/package.json`,
+        JSON.stringify(manifest(answers), null, '  ')
+      );
 
-          // github
-          $(local('gh'), [
-            'repo', '--new', answers.name, '--organization=studio-b12',
-            `--description=${answers.description}`,
-          ]);
-          $('git', [
-            'remote', 'add', 'github',
-            `git@github.com:studio-b12/${answers.name}.git`,
-          ]);
-          $('git', ['push', 'github', 'master']);
+      const npmDependencies = () => {
+        const dependencyStrings =
+          Object.keys(devDependencies).map((dep) => (
+            `${dep}@${devDependencies[dep]}`
+          ));
+        $('npm', ['install', '--save-dev', 'npm']);
+        $(local('npm'), ['install', '--save-dev'].concat(dependencyStrings));
+        $('npm', ['shrinkwrap', '--dev']);
+        return 'ok';
+      };
 
-          // git.sb12.de
-          const sb12RepoSlug = `js/lib/${answers.name}`;
-          const gitoliteAdminDir = tmp.dirSync({ unsafeCleanup: true });
-          $('git', ['clone', 'git+ssh://git@git.sb12.de/gitolite-admin.git',
-            gitoliteAdminDir.name,
-          ]);
-          $('cd', [gitoliteAdminDir.name]);
-          const confFilePath = `${gitoliteAdminDir.name}/conf/subs/js_lib.conf`;
-          const confFile = fs.readFileSync(confFilePath, 'utf8');
-          const newConfFile = confFile + u`
-            \n\trepo ${sb12RepoSlug}
-            \t\tRW+ = @webdev
-            \t\tconfig gitweb.owner = "${answers.author.replace(/\s+<.+$/, '')}"
-            \t\tconfig gitweb.description = "${
-              answers.description
-            }. Published at https://github.com/studio-b12/${answers.name} ."
-          `;
-          fs.writeFileSync(confFilePath, newConfFile);
-          $('git', ['commit',
-            `--message=New repo: ${sb12RepoSlug}`, confFilePath,
-          ])
-          $('git', ['push']);
-          $('cd', [projectRoot]);
-          gitoliteAdminDir.removeCallback();
+      const initialCommit = () => {
+        $('git', ['init']);
+        $('git', ['add', '.']);
+        $('git', ['commit', '--message=Boom!']);
+        return 'ok';
+      };
 
-          $('git', ['remote', 'add', 'origin',
-            `git@git.sb12.de:${sb12RepoSlug}.git`,
-          ]);
-          $('git', ['push', '--set-upstream', 'origin', 'master']);
+      const setupGithub = () => {
+        $(local('gh'), [
+          'repo', '--new', answers.name, '--organization=studio-b12',
+          `--description=${answers.description}`,
+        ]);
+        $('git', [
+          'remote', 'add', 'github',
+          `git@github.com:studio-b12/${answers.name}.git`,
+        ]);
+        $('git', ['push', 'github', 'master']);
+        return 'ok';
+      };
 
-          // Done!
-          process.stdout.write(
+      const setupSb12 = () => {
+        const sb12RepoSlug = `js/lib/${answers.name}`;
+        const gitoliteAdminDir = tmp.dirSync({ unsafeCleanup: true });
+        $('git', ['clone', 'git+ssh://git@git.sb12.de/gitolite-admin.git',
+          gitoliteAdminDir.name,
+        ]);
+        $('cd', [gitoliteAdminDir.name]);
+        const confFilePath = `${gitoliteAdminDir.name}/conf/subs/js_lib.conf`;
+        const confFile = fs.readFileSync(confFilePath, 'utf8');
+        const newConfFile = confFile + u`
+          \n\trepo ${sb12RepoSlug}
+          \t\tRW+ = @webdev
+          \t\tconfig gitweb.owner = "${answers.author.replace(/\s+<.+$/, '')}"
+          \t\tconfig gitweb.description = "${
+            answers.description
+          }. Published at https://github.com/studio-b12/${answers.name} ."
+        `;
+        fs.writeFileSync(confFilePath, newConfFile);
+        $('git', ['commit',
+          `--message=New repo: ${sb12RepoSlug}`, confFilePath,
+        ]);
+        $('git', ['push']);
+        $('cd', [projectRoot]);
+        gitoliteAdminDir.removeCallback();
+
+        $('git', ['remote', 'add', 'origin',
+          `git@git.sb12.de:${sb12RepoSlug}.git`,
+        ]);
+        $('git', ['push', '--set-upstream', 'origin', 'master']);
+        return 'ok';
+      };
+
+      const sayWereDone = () => {
+        process.stdout.write(
 `
 Everything set up! You can now ${bold(`cd ${answers.name}`)}
 and start your work there. Good luck!
@@ -272,11 +282,18 @@ By the way, here are things you’ll have to do by hand:
     }.
 
 `
-          );
-        },
-      ];
+        );
+      };
 
-      return fileActions.concat(repoActions);
+      return fileActions.concat([
+        enterProjectRoot,
+        packageJson,
+        npmDependencies,
+        initialCommit,
+        setupGithub,
+        setupSb12,
+        sayWereDone,
+      ]);
     },
   });
 };
---
+++ templates/js-library/(package-json).js
@@ -0,0 +1,40 @@
+module.exports = (answers) => ({
+  name: answers.name,
+  version: '0.0.0',
+  description: answers.description,
+  scripts: {
+    coverage: 'rm -rf coverage && istanbul cover test.js',
+    coveralls: 'npm run coverage && cat coverage/lcov.info | coveralls',
+    develop: 'node-dev --respawn --no-notify --no-deps test.js',
+    test: 'eslint . && node test.js',
+    reshrinkwrap: [
+      'rm -f npm-shrinkwrap.json',
+      'npm install',
+      'npm shrinkwrap --dev',
+      'git commit ' +
+        '-m \'[npm run shrinkwrap] Update dependencies\'' +
+        ' npm-shrinkwrap.json package.json',
+    ].join(' && '),
+  },
+  engines: {
+    node: '>=4.0.0',
+  },
+  dependencies: {},
+  devDependencies: {},
+  files: [
+    '/**/*.js',
+    '!/test.js',
+    '/Readme.md',
+    '/License.md',
+  ],
+  license: 'MIT',
+  keywords: answers.keywords,
+  author: `© ${answers.year} Studio B12 GmbH (http://studio-b12.de)`,
+  contributors: [
+    answers.author,
+  ],
+  repository: {
+    type: 'git',
+    url: 'https://github.com/studio-b12/${answers.name}.git',
+  },
+});
