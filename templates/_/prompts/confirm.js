336382751131f2c0f41c9a09ea7d13b041bc1ada
Barboros Can Konar <b.konar@studio-b12.de>
2016-05-26 16:11:52 +0200

    Correct usage of confirm prompt

--- templates/_/prompts/confirm.js
+++ templates/_/prompts/confirm.js
@@ -21,13 +21,15 @@ module.exports = (params) => ({
     at git@git.sb12.de:${params.slugBase}/${answers.name} .
   • Try to push stuff to \`origin\`${params.noPublic ? '' : ' and `github`'}.
 
-We’re using http://npm.im/gh for managing github repos. Make sure you have
-the right to create new repos at https://github.com/studio-b12 and that you have
-SSH access configured (More info: https://git.io/ssh-access). Make sure
-you’ve configured the \`github_user\` and \`github_token\`
-in your \`~/.gh.json\`. Otherwise, gh will ask you for credentials
-and create a github token for you. This is a security threat.
+${params.noPublic ? '' : `We’re using http://npm.im/gh for managing github
+repos. Make sure you have the right to create new repos at
+https://github.com/studio-b12 and that you have SSH access configured
+(More info: https://git.io/ssh-access). Make sure you’ve configured the
+\`github_user\` and \`github_token\` in your \`~/.gh.json\`. Otherwise, gh will
+ask you for credentials and create a github token for you.This is a security
+threat.
 
+`}
 Make sure you have read and write permissions for the repo
 git@git.sb12.com:gitolite-config.git .
 
